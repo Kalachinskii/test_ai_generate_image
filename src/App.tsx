32ca36cdf3c4001b61fd98f1e9890a994bf9e6cf
kalachinskii_vl <kalachinskii_vl@mail.ru>
2025-04-08 01:21:12 +0300

    fix разметки и стиль формы

--- src/App.tsx
+++ src/App.tsx
@@ -31,72 +31,59 @@ export function App() {
                         <button type="button" className={styles.promtBtn}>
                             <i className="fa-solid fa-dice"></i>
                         </button>
-
-                        {/* promt action/buttons */}
-                        <div className={styles.promtActions}>
-                            <div className={styles.selectWrapper}>
-                                <select
-                                    className={styles.customSelect}
-                                    required
-                                >
-                                    <option value="" selected disabled>
-                                        Выберети модель
-                                    </option>
-                                    <option value="black-forest-labs/FLUX.1-dev">
-                                        FLUX.1-dev
-                                    </option>
-                                    <option value="black-forest-lavs/FLUX.1-schenll">
-                                        FLUX.1-schenll
-                                    </option>
-                                    <option value="stabilityai/stable-diffusion-xl-base-1">
-                                        Stable Diffusion XL
-                                    </option>
-                                    <option value="runwayml/stable-diffusion-v1-5">
-                                        Stable Diffusion v1.5
-                                    </option>
-                                    <option value="prompthero/openjourney">
-                                        Openjourney
-                                    </option>
-                                </select>
-                            </div>
-
-                            <div className={styles.selectWrapper}>
-                                <select
-                                    className={styles.customSelect}
-                                    required
-                                >
-                                    <option value="" selected disabled>
-                                        Колличество
-                                    </option>
-                                    <option value="1">1 картинка</option>
-                                    <option value="2">2 картинка</option>
-                                    <option value="3">3 картинка</option>
-                                    <option value="4">4 картинка</option>
-                                </select>
-                            </div>
-
-                            <div className={styles.selectWrapper}>
-                                <select
-                                    className={styles.customSelect}
-                                    required
-                                >
-                                    <option value="" selected disabled>
-                                        Размер
-                                    </option>
-                                    <option value="1/1">Квадрат (1:1)</option>
-                                    <option value="16/9">Пеизаж (16:9)</option>
-                                    <option value="9/16">Портрет (9:16)</option>
-                                </select>
-                            </div>
-
-                            <button
-                                type="submit"
-                                className={styles.generateBtn}
-                            >
-                                <i className="fa-solid fa-wand-sparkles"></i>
-                                Сгенерировать
-                            </button>
+                    </div>
+                    {/* promt action/buttons */}
+                    <div className={styles.promtActions}>
+                        <div className={styles.selectWrapper}>
+                            <select className={styles.customSelect} required>
+                                <option value="" selected disabled>
+                                    Выберети модель
+                                </option>
+                                <option value="black-forest-labs/FLUX.1-dev">
+                                    FLUX.1-dev
+                                </option>
+                                <option value="black-forest-lavs/FLUX.1-schenll">
+                                    FLUX.1-schenll
+                                </option>
+                                <option value="stabilityai/stable-diffusion-xl-base-1">
+                                    Stable Diffusion XL
+                                </option>
+                                <option value="runwayml/stable-diffusion-v1-5">
+                                    Stable Diffusion v1.5
+                                </option>
+                                <option value="prompthero/openjourney">
+                                    Openjourney
+                                </option>
+                            </select>
                         </div>
+
+                        <div className={styles.selectWrapper}>
+                            <select className={styles.customSelect} required>
+                                <option value="" selected disabled>
+                                    Колличество
+                                </option>
+                                <option value="1">1 картинка</option>
+                                <option value="2">2 картинка</option>
+                                <option value="3">3 картинка</option>
+                                <option value="4">4 картинка</option>
+                            </select>
+                        </div>
+
+                        <div className={styles.selectWrapper}>
+                            <select className={styles.customSelect} required>
+                                <option value="" selected disabled>
+                                    Размер
+                                </option>
+                                <option value="1/1">Квадрат (1:1)</option>
+                                <option value="16/9">Пеизаж (16:9)</option>
+                                <option value="9/16">Портрет (9:16)</option>
+                            </select>
+                        </div>
+
+                        <button type="submit" className={styles.generateBtn}>
+                            <i className="fa-solid fa-wand-sparkles"></i>
+                            Сгенерировать
+                        </button>
                     </div>
                 </form>
             </div>
